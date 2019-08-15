0624fb512b0f85e1d058f176a0991f1e0ca9ea4b
Nima Fazeli <nimfazeli@gmail.com>
2019-08-15 09:37:52 -0400

    multi-run simulations

--- KukaBlockClass.py
+++ KukaBlockClass.py
@@ -9,128 +9,163 @@ import pdb
 
 
 class KukaBlock():
-    def __init__(self):
-      # connect to pybullet server
-      p.connect(p.GUI)
+    def __init__(self, withVis=True):
+        # connect to pybullet server
+        if withVis:
+            p.connect(p.GUI)
+        else:
+            p.connect(p.DIRECT)
 
-      # set additional path to find kuka model
-      p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        # set additional path to find kuka model
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-      # add plane to push on (slightly below the base of the robot)
-      self.planeId = p.loadURDF("plane.urdf", [0, 0, -0.05], useFixedBase=True)
+        # add plane to push on (slightly below the base of the robot)
+        self.planeId = p.loadURDF("plane.urdf", [0, 0, -0.05], useFixedBase=True)
 
-      # add the robot at the origin with fixed base
-      self.kukaId = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0], useFixedBase=True)
+        # add the robot at the origin with fixed base
+        self.kukaId = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0], useFixedBase=True)
 
-      # reset the base
-      p.resetBasePositionAndOrientation(self.kukaId, [0, 0, 0], [0, 0, 0, 1])
+        # reset the base
+        p.resetBasePositionAndOrientation(self.kukaId, [0, 0, 0], [0, 0, 0, 1])
 
-      # get useful robot information
-      self.kukaEndEffectorIndex = 6
-      self.numJoints = p.getNumJoints(self.kukaId)
+        # get useful robot information
+        self.kukaEndEffectorIndex = 6
+        self.numJoints = p.getNumJoints(self.kukaId)
 
-      # add the block
-      self.blockId = p.loadURDF("block_big.urdf", [-0.4, 0, .1])
-      # p.resetBasePositionAndOrientation(self.blockId, [-0.4, 0, 0.1], [0, 0, 0, 1])
-      # print(p.getNumJoints(self.blockId))
+        # add the block - we'll reset its position later
+        self.blockId = p.loadURDF("block_big.urdf", [-0.4, 0, .1])
+        # p.resetBasePositionAndOrientation(self.blockId, [-0.4, 0, 0.1], [0, 0, 0, 1])
 
-      # reset joint states to nominal pose
-      self.rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
-      for i in range(self.numJoints):
-        p.resetJointState(self.kukaId, i, self.rp[i])
-
-      # get the joint ids
-      self.jointInds = [i for i in range(self.numJoints)]
-
-      # set gravity
-      p.setGravity(0, 0, -10)
-
-      # set simulation length
-      self.simLength = 6000
-
-      # set joint damping
-      #joint damping coefficents
-      self.jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-
-      # pre-define the trajectory/force vectors
-      self.traj = np.zeros((self.simLength, 5))
-      self.contactForce = np.zeros((self.simLength, ))
-      self.contactCount = np.empty_like(self.contactForce)
-
-    def simulate(self, theta=0.1):
-
-      self.getInitConds()
-      pushStep = self.getPushStep()
-      pushDir = self.getPushDir(theta)
-
-      x = self.robotInitPoseCart[0]
-      y = self.robotInitPoseCart[1]
-      z = self.robotInitPoseCart[2]
-
-      for simTime in range(self.simLength):
-        p.stepSimulation()
-
-        # set end effector pose
-        d = pushDir * pushStep
-        x += d[0]
-        y += d[1]
-        z += d[2]
-        eePos = [x, y, z]
-
-        # compute the inverse kinematics
-        jointPoses = p.calculateInverseKinematics(self.kukaId,
-                                                  self.kukaEndEffectorIndex,
-                                                  eePos,
-                                                  self.orn,
-                                                  jointDamping=self.jd)
-
+        # reset joint states to nominal pose
+        self.rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
         for i in range(self.numJoints):
-          p.setJointMotorControl2(bodyIndex=self.kukaId,
-                                  jointIndex=i,
-                                  controlMode=p.POSITION_CONTROL,
-                                  targetPosition=jointPoses[i],
-                                  targetVelocity=0,
-                                  force=500,
-                                  positionGain=0.3,
-                                  velocityGain=1)
+            p.resetJointState(self.kukaId, i, self.rp[i])
 
-        # get joint states
-        ls = p.getLinkState(self.kukaId, self.kukaEndEffectorIndex)
-        lb = p.getLinkState(self.blockId, 0)
-        lp = p.getLinkState(self.planeId, 0)
-        print(lb)
-        print(lp)
-        pdb.set_trace()
+        # get the joint ids
+        self.jointInds = [i for i in range(self.numJoints)]
 
-        # get contact information
-        contactInfo = p.getContactPoints(self.kukaId, self.blockId)
+        # set gravity
+        p.setGravity(0, 0, -10)
 
-        # get the net contact force between robot and block
-        if len(contactInfo)>0:
-          f_c_temp = 0
-          for i in range(len(contactInfo)):
-            f_c_temp += contactInfo[i][9]
-          self.contactForce[simTime] = f_c_temp
-          self.contactCount[simTime] = len(contactInfo)
+        # set simulation length
+        self.simLength = 5000
 
-        self.traj[simTime, :] = np.array([x, y, z, x, y])
-        self.contactForce[:1000] = 0
+        # set joint damping
+        #joint damping coefficents
+        self.jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+
+        # set robot init config
+        self.robotInitPoseCart = [-0.4, -0.2, 0.01] # (x,y,z)
+        self.orn = p.getQuaternionFromEuler([0, -math.pi, 0])
+
+        # pre-define the trajectory/force vectors
+        self.traj = np.zeros((self.simLength, 5))
+        self.contactForce = np.zeros((self.simLength, ))
+        self.contactCount = np.empty_like(self.contactForce)
+
+        # reset sim time
+        self.t = 0
+
+    def simulate(self, theta=0.1, withRandom=False):
+
+        self.resetSim(withRandom)
+        pushDir = self.getPushDir(theta, withRandom)
+        pushStep = self.getPushStep()
+
+        x = self.robotInitPoseCart[0]
+        y = self.robotInitPoseCart[1]
+        z = self.robotInitPoseCart[2]
+
+        for simTime in range(self.simLength):
+            p.stepSimulation()
+
+            # set end effector pose
+            d = pushDir * pushStep
+            x += d[0]
+            y += d[1]
+            z += d[2]
+            eePos = [x, y, z]
+
+            # compute the inverse kinematics
+            jointPoses = p.calculateInverseKinematics(self.kukaId,
+                                                      self.kukaEndEffectorIndex,
+                                                      eePos,
+                                                      self.orn,
+                                                      jointDamping=self.jd)
+
+            for i in range(self.numJoints):
+                p.setJointMotorControl2(bodyIndex=self.kukaId,
+                                      jointIndex=i,
+                                      controlMode=p.POSITION_CONTROL,
+                                      targetPosition=jointPoses[i],
+                                      targetVelocity=0,
+                                      force=500,
+                                      positionGain=0.3,
+                                      velocityGain=1)
+
+            # get joint states
+            ls = p.getLinkState(self.kukaId, self.kukaEndEffectorIndex)
+            blockPose = p.getBasePositionAndOrientation(self.blockId)
+            xb = blockPose[0][0]
+            yb = blockPose[0][1]
+            roll, pitch, yaw = p.getEulerFromQuaternion(blockPose[1])
+
+            # get contact information
+            contactInfo = p.getContactPoints(self.kukaId, self.blockId)
+
+            # get the net contact force between robot and block
+            if len(contactInfo)>0:
+                f_c_temp = 0
+                for i in range(len(contactInfo)):
+                    f_c_temp += contactInfo[i][9]
+                self.contactForce[simTime] = f_c_temp
+                self.contactCount[simTime] = len(contactInfo)
+
+            self.traj[simTime, :] = np.array([x, y, xb, yb, yaw])
+
+        # contact force mask - get rid of trash in the beginning
+        self.contactForce[:300] = 0
+
         return self.traj
 
-    def getInitConds(self):
-      self.robotInitPoseCart = [-0.4, -0.2, 0.01] # (x,y,z)
-      self.t = 0
-      self.orn = p.getQuaternionFromEuler([0, -math.pi, 0])
+    def resetSim(self, withRandom):
+        # reset robot to nominal pose
+        for i in range(self.numJoints):
+            p.resetJointState(self.kukaId, i, self.rp[i])
 
-    def getPushDir(self, theta):
-      # get unit direction of the push
-      return np.array([math.sin(theta), math.cos(theta), 0.])
+        # reset block pose
+        if withRandom:
+            # define nominal block pose
+            nom_pose = np.array([-0.4, 0.0, 0.0]) # (x,y,theta)
+
+            # define uncertainty bounds
+            pos_bd = np.array([0.01, 0.01, 0.1])
+
+            # initialize array
+            blockInitPose = np.empty_like(pos_bd)
+
+            for i in range(nom_pose.shape[0]):
+                pert = np.random.uniform(-pos_bd[i], pos_bd[i])
+                blockInitPose[i] = nom_pose[i] + pert
+
+            blockInitOri = p.getQuaternionFromEuler([0, 0, blockInitPose[-1]])
+            p.resetBasePositionAndOrientation(self.blockId, [blockInitPose[0], blockInitPose[1], 0.1], blockInitOri)
+        else:
+            p.resetBasePositionAndOrientation(self.blockId, [-0.4, 0, 0.1], [0, 0, 0, 1])
+
+
+    def getPushDir(self, theta, withRandom):
+        # get unit direction of the push
+        if withRandom:
+            bd = 0.05
+            theta = np.random.uniform(low=-bd, high=bd)
+
+        return np.array([math.sin(theta), math.cos(theta), 0.])
 
     def getPushStep(self):
-      return 0.00005
+        return 0.00005
 
 
 if __name__ == "__main__":
     kb = KukaBlock()
-
     kb.simulate()
--- kukaBlock.py
+++ kukaBlock.py
@@ -1,12 +1,39 @@
 import KukaBlockClass
+import matplotlib.pyplot as plt
+import pdb
 
 
 def main():
-    kb = KukaBlockClass.KukaBlock()
+    # choose to use visualization - don't if collecting large amounts of data
+    withVis = False
+    kb = KukaBlockClass.KukaBlock(withVis)
+
+    # number of simulations to run, use 1 with visualization
+    simRuns = 5
 
     # push direction with respect to the positive y axis
-    theta = 0.0
-    traj = kb.simulate(theta)
+    # used only if random init is off
+    theta = 0.00
+    withRand = True
+
+    # plotting utility
+    axis_name = ['x robot (m)', 'y robot (m)', 'x block (m)', 'y block (m)', 'block rotation (rads)']
+    fig, axes = plt.subplots(1, 5, figsize=(18, 5))
+
+    # simulate and plot trajectories
+    for simCount in range(simRuns):
+        # simulation resets everytime it's called
+        traj = kb.simulate(theta, withRandom=withRand)
+
+        # plotting
+        for i in range(5):
+            axes[i].plot(traj[:, i])
+
+    # add some labels
+    for i in range(5):
+        axes[i].set_xlabel(axis_name[i])
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
